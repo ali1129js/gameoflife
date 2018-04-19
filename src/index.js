fa8b9ff061bfb1babb8c40ee6a97e7c1706ef702
ali1129js <ali1129js@gmail.com>
2018-04-19 16:22:21 +0200

    setup and Grid, css is a copy

--- src/index.js
+++ src/index.js
@@ -1,8 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+/**
+ * @Author: Ali Ismail
+ * @Date:   2018-04-19T16:12:59+02:00
+ * @Last modified by:   Ali Ismail
+ * @Last modified time: 2018-04-19T16:21:23+02:00
+ */
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+class Grid extends React.Component {
+  render(){
+    return(
+      <div>Grid</div>
+    )
+  }
+}
+class Main extends React.Component {
+  constructor(){
+    super()
+    this.state = {
+      generation:0
+    }
+  }
+  render(){
+    return (
+      <div>
+        <h1>The Game Of Life</h1>
+        <Grid
+        />
+        <h2>Generations: {this.state.generation} </h2>
+      </div>
+    )
+  }
+}
+ReactDOM.render(<Main />, document.getElementById('root'))
